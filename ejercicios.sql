19685f58e7b44d279f60b0fe0125fab957613e41
ramagar <rama.garcia111@gmail.com>
2024-04-04 10:54:57 -0300

    ejercicios sql

--- ejercicios.sql
+++ ejercicios.sql
@@ -1,3 +1,91 @@
+-- Active: 1693360576251@@localhost@3306@world
+###1###
+SELECT code, name, continent, population FROM country;
+
+###2###
+SELECT DISTINCT language FROM countrylanguage;
+
+###3###
+SELECT name AS Nombre, population AS Poblacion FROM city; 
+
+###4###
+SELECT name AS Nombre, GNP AS 'Producto Bruto Nacional', GNPOld AS 'Producto Bruto Nacional Anterior', (GNP - GNPOld) AS Diferencia FROM country;
+
+###5###
+SELECT name AS Nombre, Population AS Poblacion, SurfaceArea AS Superficie, (Population/SurfaceArea) AS 'Densidad Poblacional (km²)' FROM country;
+
+###6###
+SELECT * FROM country ORDER BY Population ASC;
+
+###7###
+SELECT LANGUAGE FROM countrylanguage ORDER BY Language ASC;
+
+###8###
+SELECT name, population FROM city ORDER BY Population ASC LIMIT 20;
+
+###9###
+SELECT code, name, indepyear FROM country ORDER BY `IndepYear` DESC;
+
+###10###
+SELECT name, continent FROM country ORDER BY LifeExpectancy DESC, SurfaceArea ASC LIMIT 100; 
+
+###11###
+SELECT * FROM country WHERE Population = 0;
+
+###12###
+SELECT * FROM country c WHERE LifeExpectancy > 75 ORDER BY LifeExpectancy ASC;
+
+###13###
+SELECT * FROM country c WHERE IndepYear >= 1850 AND GovernmentForm = 'Constitutional Monarchy';
+
+###14###
+SELECT * FROM country c 
+WHERE Continent = 'Europe' 
+ORDER BY GNP 
+DESC LIMIT 10;
+
+###15###
+SELECT * FROM country c WHERE Name = LocalName;
+
+###16###
+SELECT * FROM  country c WHERE IndepYear >= 1950;
+
+###17###
+SELECT * FROM country c 
+WHERE Continent IN ('Europe', 'Asia', 'South America');
+
+###18###
+SELECT * FROM country c 
+WHERE Continent != 'Africa';
+
+###19###
+SELECT * FROM city c 
+WHERE CountryCode = 'ARG' AND District != 'Buenos Aires';
+
+###20###
+SELECT * FROM city c 
+WHERE Population BETWEEN 125000 AND 130000;
+
+###21###
+SELECT * FROM country c 
+WHERE IndepYear IS NULL;
+
+###22###
+SELECT * FROM country c 
+WHERE GNPOld IS NULL AND LifeExpectancy IS NULL;
+
+###23###
+SELECT * FROM country c 
+WHERE Name LIKE 'A%';
+
+###24###
+SELECT * FROM country c 
+WHERE Name LIKE '% %';
+
+###25###
+SELECT * FROM city c 
+WHERE District LIKE '%-%' OR Name LIKE '%-%';
+
 ###26###
 SELECT country.Name AS 'Pais', city.Name AS 'Capital'
 FROM country
@@ -194,3 +282,100 @@ WHERE countrylanguage.IsOfficial = 'T'
 GROUP BY countrylanguage.`Language`
 HAVING Porcentaje <= 9
 ORDER BY Porcentaje DESC;
+
+###51###
+INSERT INTO clientes (dni, nombre, apellido, genero, fecha_nacimiento, mail,
+	movil, domicilio, barrio, nacionalidad)
+VALUES ('12169851', 'Luis Enrique', 'Pérez', 'HOMBRE', '1954-06-01',
+	'perez_luisenrique@ma1l.com', '1157319468', 'Pujol 416', 'Caballito', 'Uruguay');
+	
+###52###
+INSERT INTO clientes (dni, nombre, apellido, genero, fecha_nacimiento, mail,
+	movil, domicilio, barrio, nacionalidad)
+VALUES ('17278290', 'Florencia', 'Santos', 'MUJER', '1963-07-12', 'flor.santos@ma1l.com',
+	'1148434346', 'Piran 3015', 'Villa Urquiza', 'Argentina'); #ETC
+	
+###53###
+INSERT INTO clientes (dni, nombre, apellido, genero, fecha_nacimiento, mail,
+	movil, domicilio, barrio, nacionalidad)
+VALUES ('36427254', 'María Sol', 'González', 'MUJER', '1992-08-15', 'ma.gon54@gma1l.com',
+	'1136782013', 'Gurruchaga 243', 'Villa Crespo', 'Argentina');
+
+###54###
+UPDATE clientes
+SET apellido = 'Chaves'
+WHERE id_cliente = '85';
+
+#Corroboramos
+SELECT * FROM clientes c 
+WHERE id_cliente  = '85';
+
+###55###
+UPDATE clientes 
+SET dni = concat('0', dni) 
+WHERE LENGTH(dni) = 7;
+
+###56###
+UPDATE clientes
+SET nombre = 'Nicolás'
+WHERE nombre = 'Nicolas';
+
+SELECT * FROM clientes
+WHERE nombre = 'Nicolás'
+	OR nombre = 'Nicolas';
+
+###57###
+UPDATE clientes
+SET apellido = upper(apellido)
+WHERE nacionalidad != 'Argentina';
+
+###58###
+DELETE FROM clientes
+WHERE id_cliente = 9800;
+
+###59###
+DELETE FROM clientes 
+WHERE mail LIKE '%z0h0%';
+
+###60###
+DELETE FROM clientes
+WHERE clientes.barrio = 'Palermo';
+
+CREATE TABLE IF NOT EXISTS nivel(
+nivel_id int AUTO_INCREMENT PRIMARY KEY,
+nivel VARCHAR (3)
+);
+
+CREATE TABLE IF NOT EXISTS genero(
+genero_id int AUTO_INCREMENT PRIMARY KEY,
+genero varchar (20)
+);
+
+CREATE TABLE IF NOT EXISTS instructores(
+instructor_id int AUTO_INCREMENT PRIMARY KEY,
+apellido varchar (20),
+nombre varchar (20),
+email varchar (45)
+);
+
+CREATE TABLE IF NOT EXISTS alumnos(
+legajo int AUTO_INCREMENT PRIMARY KEY,
+apellido varchar (50),
+nombre varchar (50),
+email varchar (50),
+genero_id int,
+nota_1p decimal (4, 2),
+nota_2p decimal (4, 2),
+nota_final decimal (4, 2),
+nivel_id int,
+instructor_id int,
+FOREIGN KEY (genero_id) REFERENCES genero (genero_id),
+FOREIGN KEY (nivel_id) REFERENCES nivel (nivel_id),
+FOREIGN KEY (instructor_id) REFERENCES instructores (instructor_id)
+);
+
+
+
+
+
+
